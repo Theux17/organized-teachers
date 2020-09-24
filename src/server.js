95d6af0692d383fc3d6f33eb46f35b8a0699fd02
Theux17 <oliveira.ms18@gmail.com>
2020-09-24 19:45:15 -0300

    Instalação/configuração do express-session

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
+const session = require('./config/session')
 
 const app = express()
 
+app.use(session)
 
 app.use(express.urlencoded({extended: true }))
 app.use(express.json())
@@ -10,4 +12,10 @@ app.use(routes)
 
 app.listen(3333, () => {
     console.log("Server is running")
+})
+
+app.use((req, res) => {
+    res.status(404).json({
+        error: "Rota não encontrada"
+    })
 })
