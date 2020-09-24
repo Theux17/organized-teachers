91301b0f1a8180633682f1b1f8a5fdb700ad83d1
Theux17 <oliveira.ms18@gmail.com>
2020-09-24 20:25:57 -0300

    Configuração para utilizar classe

--- src/app/validators/teacher.js
+++ src/app/validators/teacher.js
@@ -1,15 +1,78 @@
+const { compare } = require('bcryptjs')
+
 const Teacher = require('../models/Teacher')
 
-async function checksIfExistsTeacher(req, res, next) {
-    const id = req.params.id
+module.exports = class TeacherValidador {
+    async teacher(req, res, next) {
+        try {
+            const { email } = req.body
 
-    const teacher = await Teacher.findOne({ where: { id } })
-    
-    if(!teacher) return res.status(204).json({
-        error: "Professor não encontrado"
-    })
+            const teachers = await Teacher.findAll()
 
-    next()
+            // Busca os professores cadastrados
+            const registeredTeachers = teachers.find(teacher => {
+                return teacher.id != req.session.teacherId
+            })
+
+            if (registeredTeachers.id != req.session.teacherId && registeredTeachers.email == email)
+                return res.status(401).json({
+                    message: "Email já cadastrado"
+                })
+
+            const teacher = await Teacher.findOne({ where: { id: req.params.id } })
+
+            if (!teacher) return res.status(204).json({
+                error: "Professor não encontrado"
+            })
+
+            if (req.session.teacherId != req.params.id) return res.status(401).json({
+                error: "Acesso negado!"
+            })
+
+            if (teacher && req.body.password) {
+
+                const passed = await compare(req.body.password, teacher.password)
+
+                if (!passed) return res.json({
+                    error: "Senha incorreta"
+                })
+            }
+
+            next()
+
+        } catch (error) {
+            return res.status(400).json({
+                error: "Erro inesperado aconteceu"
+            })
+        }
+
+
+    }
+    async create(req, res, next) {
+        try {
+            if (req.session.teacherId) return res.status(400).json({
+                message: "Você não pode cadastrar um novo usuário nesse momento"
+            })
+
+            const { email } = req.body
+
+            const teachers = await Teacher.findAll()
+            const registeredTeachers = teachers.find(teacher => {
+                return teacher.email == email
+            })
+            
+            if (registeredTeachers && registeredTeachers.email  == email) return res.status(401).json({
+                error: "Usuário já cadastrado"
+            })
+
+            next()
+        
+        } catch (error) {
+            console.log(error);
+            
+            return res.status(400).json({
+                error: "Erro inesperado aconteceu"
+            })
+        }
+    }
 }
-
-module.exports = { checksIfExistsTeacher }
--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -2,14 +2,28 @@ const express = require('express')
 const routes = express.Router()
 
 const TeacherController = require('../app/controllers/TeacherController')
+const SessionController = require('../app/controllers/SessionController')
+
+const SessionValidator = require('../app/validators/session')
+const TeacherValidator = require('../app/validators/teacher')
+
+const Session = require('../app/middlewares/session')
+const { usersLoggedIn } = new Session
 
 const teacherController = new TeacherController
-const { checksIfExistsTeacher } = require('../app/validators/teacher')
+const sessionController = new SessionController
 
-routes.get('', teacherController.index)
-routes.post('', teacherController.post)
-routes.get('/show/:id', checksIfExistsTeacher, teacherController.show)
-routes.put('/:id', checksIfExistsTeacher, teacherController.put)
-routes.delete('/:id', checksIfExistsTeacher, teacherController.delete)
+const sessionValidator = new SessionValidator
+const teacherValidator = new TeacherValidator
+
+
+routes.post('/login', sessionValidator.login, sessionController.login )
+routes.post('/logout', usersLoggedIn, sessionValidator.logout, sessionController.logout )
+
+routes.post('', teacherValidator.create, teacherController.post)
+routes.get('/profile/:id', usersLoggedIn, teacherValidator.teacher, teacherController.show)
+routes.put('/:id', usersLoggedIn, teacherValidator.teacher, teacherController.put)
+routes.delete('/:id', usersLoggedIn, teacherValidator.teacher, teacherController.delete)
+
 
 module.exports = routes
