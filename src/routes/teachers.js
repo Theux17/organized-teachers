e204169ff30e2137768f2cd54032266c0e20d3ee
Theux17 <oliveira.ms18@gmail.com>
2020-09-25 18:36:25 -0300

    Rota dos alunos

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -3,6 +3,9 @@ const routes = express.Router()
 
 const TeacherController = require('../app/controllers/TeacherController')
 const SessionController = require('../app/controllers/SessionController')
+//const ActivitiesController = require('../app/controllers/ActivitiesController')
+const StudentsController = require('../app/controllers/StudentsController')
+
 
 const SessionValidator = require('../app/validators/session')
 const TeacherValidator = require('../app/validators/teacher')
@@ -12,18 +15,27 @@ const { usersLoggedIn } = new Session
 
 const teacherController = new TeacherController
 const sessionController = new SessionController
+const studentsController = new StudentsController
+// const activitiesController = new ActivitiesController
 
 const sessionValidator = new SessionValidator
 const teacherValidator = new TeacherValidator
 
-
 routes.post('/login', sessionValidator.login, sessionController.login )
 routes.post('/logout', usersLoggedIn, sessionValidator.logout, sessionController.logout )
 
+// teachers
 routes.post('', teacherValidator.create, teacherController.post)
 routes.get('/profile/:id', usersLoggedIn, teacherValidator.teacher, teacherController.show)
 routes.put('/:id', usersLoggedIn, teacherValidator.teacher, teacherController.put)
 routes.delete('/:id', usersLoggedIn, teacherValidator.teacher, teacherController.delete)
+
+// students
+routes.post('/students', usersLoggedIn, studentsController.post)
+routes.get('/students/:id', usersLoggedIn, studentsController.show)
+routes.put('/students/:id', usersLoggedIn, studentsController.put )
+routes.delete('/students/:id', usersLoggedIn, studentsController.delete )
+
 
 
 module.exports = routes
